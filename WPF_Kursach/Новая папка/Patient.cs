965d69a3c459aa05b559af90d1567908ad531515
Ko4erizhkas <fit12347@gmail.com>
2024-12-11 19:51:38 +0700

    Добавил поля для класса Patient и Doctor

---
+++ WPF_Kursach/Новая папка/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Kursach
+{
+    public class Patient
+    {
+        public string Id { get; set; } // Идентификатор
+        public string FullName { get; set; } // Полное имя
+        public DateTime DateBirth { get; set; } // Дата рождения
+        public string Gender { get; set; } 
+        public string Address { get; set; } // Адрес проживания
+        public string PhoneNumber { get; set; } // Номер телефона
+        public string MedicalInstitution { get; set; } // Название больницы или поликлиники
+        public Doctor CurrentDoctor { get; set; } // Лечащий врач
+    }
+}
