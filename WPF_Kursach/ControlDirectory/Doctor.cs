7469b491ae9b459c6ccf27cb4d7b36834d77b04a
Ko4erizhkas <fit12347@gmail.com>
2024-12-14 23:44:41 +0700

    Добавил Form и директории(ActionForms, AuthForms) для них  - SelectAction, AuthDoctorForm, AuthPatientForm,
    cw
    c
    cwwq
    
    cw
    cw
    c
    w
    wq
    wq
    +wq
    
    ZZ
    ZZZZ

--- WPF_Kursach/ControlDirectory/Doctor.cs
+++ WPF_Kursach/ControlDirectory/Doctor.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace WPF_Kursach.ControlDirectory
@@ -9,11 +11,21 @@ namespace WPF_Kursach.ControlDirectory
     public class Doctor
     {
         private int _ExpYears;
-        public string Id { get; set; }
-        public string FullName { get; set; }
-        public string Surname { get; set; } // Фамилия
-        public string Specialty { get; set; }
-        public string PhoneNumber { get; set; }
+        public string? Id { get; set; }
+
+        [JsonPropertyName("FullName")]
+        public string? FullName { get; set; }
+
+        [JsonPropertyName("Surname")]
+        public string? Surname { get; set; } // Фамилия
+
+        [JsonPropertyName("Specialty")]
+        public string? Specialty { get; set; }
+
+        [JsonPropertyName("PhoneNumber")]
+        public string? PhoneNumber { get; set; }
+
+        [JsonPropertyName("ExperienceYears")]
         public int ExperienceYears
         {
             get 
@@ -45,6 +57,44 @@ namespace WPF_Kursach.ControlDirectory
             this.Specialty = _Speciality;
             this.PhoneNumber = _PhoneNumber;
             this.ExperienceYears = _ExperienceYears;
+        }
+        public Doctor() { }
+        public string SerializeDoctor(string _Surname, string _FullName,
+                      string _Speciality, int _ExperienceYears,
+                      string _PhoneNumber)
+        {
+            string jsonDataDoctor = JsonSerializer.Serialize<Doctor>(new Doctor (_Surname, _FullName, _Speciality, _ExperienceYears, _PhoneNumber));
+            return jsonDataDoctor;
+        }
+        public string SerializeDoctor(Doctor doctor)
+        {
+            string jsonDataDoctor = JsonSerializer.Serialize<Doctor>(doctor);
+            return jsonDataDoctor;
+        }
+        public Doctor DeserializeDoctor(string? fileName)
+        {
+            if (fileName == null)
+            {
+                return null;
+            }
+            else 
+            {
+                Doctor DJsonDataDoctor = JsonSerializer.Deserialize<Doctor>(fileName);
+                return DJsonDataDoctor;
+            }   
+        }
+        public void Main_Doctor()
+        {
+            Doctor d = new Doctor
+            {
+                FullName = "asd",
+                Surname = "shadg",
+                Specialty = " sidghla",
+                ExperienceYears = 123,
+                PhoneNumber = "125681364"
+            };
+            string dop = SerializeDoctor(d);
+            var es = DeserializeDoctor(dop);
         }
     }
     
